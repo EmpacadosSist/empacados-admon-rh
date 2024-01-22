a14f2fe95d0d07d4022e6cc2c691abfa169623f4
EmpacadosSist <140020926+EmpacadosSist@users.noreply.github.com>
2024-01-22 11:24:45 -0600

    Delete conexion directory

--- conexion/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn=new mysqli('localhost:3306','root','Ssystem#32','rh_admon');
-
-//la siguiente linea de codigo es para ver los acentos y las ñ
-$conn->set_charset('utf8');
-if ($conn->connect_errno) {
-  echo "Error al conectarse con My SQL debido al error".$conn->connect_error;
-} 
-
