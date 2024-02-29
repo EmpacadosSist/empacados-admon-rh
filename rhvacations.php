43c1d9067242c1a3dd8f8ec052f1a2df616d9ab1
EmpacadosSist1 <sistemas@empacados.com>
2024-02-29 09:57:17 -0600

    diseño de html

--- rhvacations.php
+++ rhvacations.php
@@ -91,16 +91,152 @@ body {
            <br>
     <form id="form_id" method="post" action="pdfpuesto.php">
         <h2  align="" id="title" class="animate__animated animate__bounceInDown card-title">
-          
                 Datos de Empleado
            <img src="assets/img/empleadoempacados.png" alt="" width="60">
-          </h2>
+        </h2>
      <div class="form-row">
-  
+     <table class="mdl-data-table" id="myTable" style="font-size:74%;">
+       <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Cargo</th>
+            <th>Oficina</th>
+            <th>Edad</th>
+            <th>Fecha de inicio</th>
+            <th>Salario</th>
+        </tr>
+        </thead>
+         <tbody>
+            <tr>
+
+                <td><a href="prueba1.php"><img src="assets/img/idempleado.png" width="35" height="35" alt="mas"></a>Kimberly Blanco</td>
+                <td>System Architect</td>
+                <td>Edinburgh</td>
+                <td>61</td>
+                <td>2011-04-25</td>
+                <td>$320,800</td>
+            </tr>
+            <tr>
+                <td><img src="assets/img/idempleado.png"  style="max-height: 30px;">Kimberly Blanco</td>
+                <td>Accountant</td>
+                <td>Tokyo</td>
+                <td>63</td>
+                <td>2011-07-25</td>
+                <td>$170,750</td>
+            </tr>
+            <tr>
+                 <td><img src="assets/img/idempleado.png"  style="max-height: 30px;">Kimberly Blanco</td>
+                <td>Junior Technical Author</td>
+                <td>San Francisco</td>
+                <td>66</td>
+                <td>2009-01-12</td>
+                <td>$86,000</td>
+            </tr>
+            <tr>
+                  <td><img src="assets/img/idempleado.png"  style="max-height: 30px;">Kimberly Blanco</td>
+                <td>Senior Javascript Developer</td>
+                <td>Edinburgh</td>
+                <td>22</td>
+                <td>2012-03-29</td>
+                <td>$433,060</td>
+            </tr>
+            <tr>
+                  <td><img src="assets/img/idempleado.png"  style="max-height: 30px;">Kimberly Blanco</td>
+                <td>Accountant</td>
+                <td>Tokyo</td>
+                <td>33</td>
+                <td>2008-11-28</td>
+                <td>$162,700</td>
+            </tr>
+            <tr>
+                  <td><img src="assets/img/idempleado.png"  style="max-height: 30px;">Kimberly Blanco</td>
+                <td>Integration Specialist</td>
+                <td>New York</td>
+                <td>61</td>
+                <td>2012-12-02</td>
+                <td>$372,000</td>
+            </tr>
+            <tr>
+                  <td><img src="assets/img/idempleado.png"  style="max-height: 30px;">Kimberly Blanco</td>
+                <td>Sales Assistant</td>
+                <td>San Francisco</td>
+                <td>59</td>
+                <td>2012-08-06</td>
+                <td>$137,500</td>
+            </tr>
+            <tr>
+                  <td><img src="assets/img/idempleado.png"  style="max-height: 30px;">Kimberly Blanco</td>
+                <td>Integration Specialist</td>
+                <td>Tokyo</td>
+                <td>55</td>
+                <td>2010-10-14</td>
+                <td>$327,900</td>
+            </tr>
+            <tr>
+                  <td><img src="assets/img/idempleado.png"  style="max-height: 30px;">Kimberly Blanco</td>
+                <td>Javascript Developer</td>
+                <td>San Francisco</td>
+                <td>39</td>
+                <td>2009-09-15</td>
+                <td>$205,500</td>
+            </tr>
+            <tr>
+                  <td><img src="assets/img/idempleado.png"  style="max-height: 30px;">Kimberly Blanco</td>
+                <td>Software Engineer</td>
+                <td>Edinburgh</td>
+                <td>23</td>
+                <td>2008-12-13</td>
+                <td>$103,600</td>
+            </tr>
+            <tr>
+                  <td><img src="assets/img/idempleado.png"  style="max-height: 30px;">Kimberly Blanco</td>
+                <td>Office Manager</td>
+                <td>London</td>
+                <td>30</td>
+                <td>2008-12-19</td>
+                <td>$90,560</td>
+            </tr>
+            <tr>
+                  <td><img src="assets/img/idempleado.png"  style="max-height: 30px;">Roberto Reyes</td>
+                <td>Support Lead</td>
+                <td>Edinburgh</td>
+                <td>22</td>
+                <td>2013-03-03</td>
+                <td>$342,000</td>
+            </tr>
+            <tr>
+                  <td><img src="assets/img/idempleado.png"  style="max-height: 30px;">Roberto Reyes</td>
+                <td>Regional Director</td>
+                <td>San Francisco</td>
+                <td>36</td>
+                <td>2008-10-16</td>
+                <td>$470,600</td>
+            </tr>
+            <tr>
+                  <td><img src="assets/img/idempleado.png"  style="max-height: 30px;">Roberto Reyes</td>
+                <td>Senior Marketing Designer</td>
+                <td>London</td>
+                <td>43</td>
+                <td>2012-12-18</td>
+                <td>$313,500</td>
+            </tr>
+            <tr>
+                  <td><img src="assets/img/idempleado.png"  style="max-height: 30px;">Roberto Reyes</td>
+                <td>Regional Director</td>
+                <td>London</td>
+                <td>19</td>
+                <td>2010-03-17</td>
+                <td>$385,750</td>
+            </tr>
+          
+        </tbody>
+      
+    </table>
+          
 
 
-</div>
-</div>
+    </div>
+    </div>
 
 
 
