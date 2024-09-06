239e246ce1319260f3809b8938323f396a47a4ab
EmpacadosSist1 <sistemas@empacados.com>
2024-09-06 17:09:06 -0600

    se realizo la consulta del organigrama desde la base de datos

--- rhorganigramaadmon.php
+++ rhorganigramaadmon.php
@@ -1,6 +1,7 @@
 <?php 
   require_once('layout/session.php');
   require_once('helpers/utils.php');
+  require_once('helpers/Consultas.php');
 ?>
 <?php require 'layout/libreriasdatatable.php';?>
 <?php require 'nav.php'; ?>
@@ -90,8 +91,7 @@ var chart = new OrgChart(document.getElementById("tree"), {
     tags: {
         filter: {
             template: 'dot',
-                        template: 'filtered'
-
+            template: 'filtered'
         }
     }
 });
@@ -168,61 +168,68 @@ chart.filterUI.on('show-items', function(sender, args){
     }
 });
 
-
 chart.onInit(function(args){
     this.filterUI.show('title');
 });
 
+<?php $user = Consultas::listUsers($conn); ?>
 
 chart.load([
 
-    { id: "1", name: "Pedro Chapa Chavez", title: "Director General", email: "amber@domain.com", img: "https://cdn.balkan.app/shared/1.jpg" },
-    { id: "2", pid: "1", name: "Kimberly Michel", title: "Consultor SAP", email: "sistemas@empacados.com", img: "https://cdn.balkan.app/shared/2.jpg" },
-    { id: "3", pid: "2", tags: ['partner'], name: "Janae Barrett", title: "Technical Director", img: "https://cdn.balkan.app/shared/3.jpg" },
-    { id: "4", pid: "1", name: "Aaliyah Webb", title: "Manager", email: "jay@domain.com", img: "https://cdn.balkan.app/shared/4.jpg" },
-    { id: "5", pid: "1", name: "Aaliyah Webb", title: "Manager", email: "jay@domain.com", img: "https://cdn.balkan.app/shared/4.jpg" },
-    { id: "6", pid: "2", name: "Elliot Ross", title: "QA", img: "https://cdn.balkan.app/shared/5.jpg" },
-    { id: "7", pid: "2", name: "Anahi Gordon", title: "QA", img: "https://cdn.balkan.app/shared/6.jpg" },
-    { id: "8", pid: "2", name: "Knox Macias", title: "QA", img: "https://cdn.balkan.app/shared/7.jpg" },
-    { id: "9", pid: "3", name: "Nash", title: ".NET Team Lead", email: "kohen@domain.com", img: "https://cdn.balkan.app/shared/8.jpg" },
-    { id: "10", pid: "8", name: "Alice Gray", title: "Programmer", img: "https://cdn.balkan.app/shared/10.jpg" },
-    { id: "11", pid: "8", name: "Anne Ewing", title: "Programmer", img: "https://cdn.balkan.app/shared/11.jpg" },
-    { id: "12", pid: "9", name: "Reuben Mcleod", title: "Programmer", img: "https://cdn.balkan.app/shared/12.jpg" },
-    { id: "13", pid: "9", name: "Ariel Wiley", title: "Programmer", img: "https://cdn.balkan.app/shared/4.jpg" },
-    { id: "14", pid: "4", name: "Lucas West", title: "Marketer", img: "https://cdn.balkan.app/shared/14.jpg" },
-    { id: "15", pid: "4", name: "Adan Travis", title: "Designer", img: "https://cdn.balkan.app/shared/15.jpg" },
-    { id: "16", pid: "4", name: "Alex Snider", title: "Sales Manager", img: "https://cdn.balkan.app/shared/4.jpg" },
-    { id: "17", pid: "4", name: "Ella Pratt", title: "Marketing Assistant", img: "https://cdn.balkan.app/shared/17.jpg" },
-    { id: "18", pid: "4", name: "Aliyah Soto", title: "Marketing Assistant", img: "https://cdn.balkan.app/shared/18.jpg" },
-    { id: "19", pid: "4", name: "Laylah Benson", title: "Marketing Assistant", img: "https://cdn.balkan.app/shared/19.jpg" },
-    { id: "20", pid: "4", name: "Paxton Doyle", title: "Marketing Assistant", img: "https://cdn.balkan.app/shared/4.jpg" },
-    { id: "21", pid: "4", name: "Lyric Odonnell", title: "Marketing Assistant", img: "https://cdn.balkan.app/shared/21.jpg" },
-    { id: "22", pid: "6", name: "Rylan Johnson", title: "QA Analyst", img: "https://cdn.balkan.app/shared/5.jpg" },
-    { id: "23", pid: "6", name: "Tyrell Wood", title: "QA Analyst", img: "https://cdn.balkan.app/shared/6.jpg" },
-    { id: "24", pid: "7", name: "Alfonso Barry", title: "QA Analyst", img: "https://cdn.balkan.app/shared/7.jpg" },
-    { id: "25", pid: "7", name: "Trevin Mcconnell", title: "QA Analyst", img: "https://cdn.balkan.app/shared/8.jpg" },
-    { id: "26", pid: "8", name: "Jude Rowland", title: "Senior QA", img: "https://cdn.balkan.app/shared/9.jpg" },
-    { id: "27", pid: "8", name: "Colt Velez", title: "Senior QA", img: "https://cdn.balkan.app/shared/10.jpg" },
-    { id: "28", pid: "9", name: "Elaine Briggs", title: "Senior .NET Developer", img: "https://cdn.balkan.app/shared/3.jpg" },
-    { id: "29", pid: "9", name: "Yadiel Green", title: "Senior .NET Developer", img: "https://cdn.balkan.app/shared/8.jpg" },
-    { id: "30", pid: "10", name: "Kiera Mcdaniel", title: "Junior Programmer", img: "https://cdn.balkan.app/shared/7.jpg" },
-    { id: "31", pid: "10", name: "Jadon Pugh", title: "Junior Programmer", img: "https://cdn.balkan.app/shared/5.jpg" },
-    { id: "32", pid: "11", name: "Kai Leblanc", title: "Junior Programmer", img: "https://cdn.balkan.app/shared/8.jpg" },
-    { id: "33", pid: "11", name: "Emmett Bender", title: "Junior Programmer", img: "https://cdn.balkan.app/shared/9.jpg" },
-    { id: "34", pid: "12", name: "Jesse Pacheco", title: "Senior Programmer", img: "https://cdn.balkan.app/shared/10.jpg" },
-    { id: "35", pid: "12", name: "Jazmin Velasquez", title: "Senior Programmer", img: "https://cdn.balkan.app/shared/16.jpg" },
-    { id: "36", pid: "13", name: "Chase Miranda", title: "Junior Programmer", img: "https://cdn.balkan.app/shared/17.jpg" },
-    { id: "37", pid: "13", name: "Gracelyn Delacruz", title: "Junior Programmer", img: "https://cdn.balkan.app/shared/18.jpg" },
-    { id: "46", pid: "13", name: "Gracelyn Delacruz", title: "Junior Programmer", img: "https://cdn.balkan.app/shared/18.jpg" },
-    { id: "47", pid: "13", name: "Gracelyn Delacruz", title: "Junior Programmer", img: "https://cdn.balkan.app/shared/18.jpg" },
-    { id: "38", pid: "14", name: "Phoenix Rivers", title: "Marketing Assistant", img: "https://cdn.balkan.app/shared/19.jpg" },
-    { id: "39", pid: "14", name: "Rayan Patel", title: "Marketing Assistant", img: "https://cdn.balkan.app/shared/16.jpg" },
-    { id: "40", pid: "15", name: "Ariella Donovan", title: "Graphic Designer", img: "https://cdn.balkan.app/shared/5.jpg" },
-    { id: "41", pid: "15", name: "Makenna Mcfarland", title: "Graphic Designer", img: "https://cdn.balkan.app/shared/6.jpg" },
-    { id: "42", pid: "16", name: "Rory Rivas", title: "Sales Representative", img: "https://cdn.balkan.app/shared/7.jpg" },
-    { id: "43", pid: "16", name: "Sloane Mcbride", title: "Sales Representative", img: "https://cdn.balkan.app/shared/8.jpg" },
-    { id: "44", pid: "1", name: "Roberto Reyes", title: "Sales Representative", img: "https://cdn.balkan.app/shared/11.jpg" },
-    { id: "45", pid: "44", name: "Roberto Reyes", title: "Sales Representative", img: "https://cdn.balkan.app/shared/12.jpg" },
+    <?php
+    for($i = 0; $i < count($user); $i++){ ?>
+        { id: "<?=$user[$i]['usuarioId'] ?>", pid: "<?=$user[$i]['superuserId'] ?>", name: "<?=$user[$i]['nombre']," ", $user[$i]['apellido1'], " ", $user[$i]['apellido2']?> ", title: "<?= $user[$i]['puesto']?>"},
+    <?php } ?>
+
+
+
+    // { id: "1", name: "Pedro Chapa Chavez", title: "Director General", email: "amber@domain.com", img: "https://cdn.balkan.app/shared/1.jpg" },
+    // { id: "2", pid: "1", name: "Kimberly Michel", title: "Consultor SAP", email: "sistemas@empacados.com", img: "https://cdn.balkan.app/shared/2.jpg" },
+    // { id: "3", pid: "2", tags: ['partner'], name: "Janae Barrett", title: "Technical Director", img: "https://cdn.balkan.app/shared/3.jpg" },
+    // { id: "4", pid: "1", name: "Aaliyah Webb", title: "Manager", email: "jay@domain.com", img: "https://cdn.balkan.app/shared/4.jpg" },
+    // { id: "5", pid: "1", name: "Aaliyah Webb", title: "Manager", email: "jay@domain.com", img: "https://cdn.balkan.app/shared/4.jpg" },
+    // { id: "6", pid: "2", name: "Elliot Ross", title: "QA", img: "https://cdn.balkan.app/shared/5.jpg" },
+    // { id: "7", pid: "2", name: "Anahi Gordon", title: "QA", img: "https://cdn.balkan.app/shared/6.jpg" },
+    // { id: "8", pid: "2", name: "Knox Macias", title: "QA", img: "https://cdn.balkan.app/shared/7.jpg" },
+    // { id: "9", pid: "3", name: "Nash", title: ".NET Team Lead", email: "kohen@domain.com", img: "https://cdn.balkan.app/shared/8.jpg" },
+    // { id: "10", pid: "8", name: "Alice Gray", title: "Programmer", img: "https://cdn.balkan.app/shared/10.jpg" },
+    // { id: "11", pid: "8", name: "Anne Ewing", title: "Programmer", img: "https://cdn.balkan.app/shared/11.jpg" },
+    // { id: "12", pid: "9", name: "Reuben Mcleod", title: "Programmer", img: "https://cdn.balkan.app/shared/12.jpg" },
+    // { id: "13", pid: "9", name: "Ariel Wiley", title: "Programmer", img: "https://cdn.balkan.app/shared/4.jpg" },
+    // { id: "14", pid: "4", name: "Lucas West", title: "Marketer", img: "https://cdn.balkan.app/shared/14.jpg" },
+    // { id: "15", pid: "4", name: "Adan Travis", title: "Designer", img: "https://cdn.balkan.app/shared/15.jpg" },
+    // { id: "16", pid: "4", name: "Alex Snider", title: "Sales Manager", img: "https://cdn.balkan.app/shared/4.jpg" },
+    // { id: "17", pid: "4", name: "Ella Pratt", title: "Marketing Assistant", img: "https://cdn.balkan.app/shared/17.jpg" },
+    // { id: "18", pid: "4", name: "Aliyah Soto", title: "Marketing Assistant", img: "https://cdn.balkan.app/shared/18.jpg" },
+    // { id: "19", pid: "4", name: "Laylah Benson", title: "Marketing Assistant", img: "https://cdn.balkan.app/shared/19.jpg" },
+    // { id: "20", pid: "4", name: "Paxton Doyle", title: "Marketing Assistant", img: "https://cdn.balkan.app/shared/4.jpg" },
+    // { id: "21", pid: "4", name: "Lyric Odonnell", title: "Marketing Assistant", img: "https://cdn.balkan.app/shared/21.jpg" },
+    // { id: "22", pid: "6", name: "Rylan Johnson", title: "QA Analyst", img: "https://cdn.balkan.app/shared/5.jpg" },
+    // { id: "23", pid: "6", name: "Tyrell Wood", title: "QA Analyst", img: "https://cdn.balkan.app/shared/6.jpg" },
+    // { id: "24", pid: "7", name: "Alfonso Barry", title: "QA Analyst", img: "https://cdn.balkan.app/shared/7.jpg" },
+    // { id: "25", pid: "7", name: "Trevin Mcconnell", title: "QA Analyst", img: "https://cdn.balkan.app/shared/8.jpg" },
+    // { id: "26", pid: "8", name: "Jude Rowland", title: "Senior QA", img: "https://cdn.balkan.app/shared/9.jpg" },
+    // { id: "27", pid: "8", name: "Colt Velez", title: "Senior QA", img: "https://cdn.balkan.app/shared/10.jpg" },
+    // { id: "28", pid: "9", name: "Elaine Briggs", title: "Senior .NET Developer", img: "https://cdn.balkan.app/shared/3.jpg" },
+    // { id: "29", pid: "9", name: "Yadiel Green", title: "Senior .NET Developer", img: "https://cdn.balkan.app/shared/8.jpg" },
+    // { id: "30", pid: "10", name: "Kiera Mcdaniel", title: "Junior Programmer", img: "https://cdn.balkan.app/shared/7.jpg" },
+    // { id: "31", pid: "10", name: "Jadon Pugh", title: "Junior Programmer", img: "https://cdn.balkan.app/shared/5.jpg" },
+    // { id: "32", pid: "11", name: "Kai Leblanc", title: "Junior Programmer", img: "https://cdn.balkan.app/shared/8.jpg" },
+    // { id: "33", pid: "11", name: "Emmett Bender", title: "Junior Programmer", img: "https://cdn.balkan.app/shared/9.jpg" },
+    // { id: "34", pid: "12", name: "Jesse Pacheco", title: "Senior Programmer", img: "https://cdn.balkan.app/shared/10.jpg" },
+    // { id: "35", pid: "12", name: "Jazmin Velasquez", title: "Senior Programmer", img: "https://cdn.balkan.app/shared/16.jpg" },
+    // { id: "36", pid: "13", name: "Chase Miranda", title: "Junior Programmer", img: "https://cdn.balkan.app/shared/17.jpg" },
+    // { id: "37", pid: "13", name: "Gracelyn Delacruz", title: "Junior Programmer", img: "https://cdn.balkan.app/shared/18.jpg" },
+    // { id: "46", pid: "13", name: "Gracelyn Delacruz", title: "Junior Programmer", img: "https://cdn.balkan.app/shared/18.jpg" },
+    // { id: "47", pid: "13", name: "Gracelyn Delacruz", title: "Junior Programmer", img: "https://cdn.balkan.app/shared/18.jpg" },
+    // { id: "38", pid: "14", name: "Phoenix Rivers", title: "Marketing Assistant", img: "https://cdn.balkan.app/shared/19.jpg" },
+    // { id: "39", pid: "14", name: "Rayan Patel", title: "Marketing Assistant", img: "https://cdn.balkan.app/shared/16.jpg" },
+    // { id: "40", pid: "15", name: "Ariella Donovan", title: "Graphic Designer", img: "https://cdn.balkan.app/shared/5.jpg" },
+    // { id: "41", pid: "15", name: "Makenna Mcfarland", title: "Graphic Designer", img: "https://cdn.balkan.app/shared/6.jpg" },
+    // { id: "42", pid: "16", name: "Rory Rivas", title: "Sales Representative", img: "https://cdn.balkan.app/shared/7.jpg" },
+    // { id: "43", pid: "16", name: "Sloane Mcbride", title: "Sales Representative", img: "https://cdn.balkan.app/shared/8.jpg" },
+    // { id: "44", pid: "1", name: "Roberto Reyes", title: "Sales Representative", img: "https://cdn.balkan.app/shared/11.jpg" },
+    // { id: "45", pid: "44", name: "Roberto Reyes", title: "Sales Representative", img: "https://cdn.balkan.app/shared/12.jpg" },
 
 ]);
 
