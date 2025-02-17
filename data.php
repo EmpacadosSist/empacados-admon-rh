df4ade7bf6395b8ddead75224d9907fd536ab069
EmpacadosSist1 <sistemas@empacados.com>
2025-02-17 11:22:12 -0600

    se agrego la iteracion por puesto

--- data.php
+++ data.php
@@ -1,6 +1,10 @@
 <?php
-  $user = Consultas::listUsersImage($conn);
-  $userlvl0 = Consultas::listUserByLevel($conn);
+// PuestoSup
+// puestoId
+// usuarioId
+
+  $user = Consultas::listPositionImage($conn);
+  $userlvl0 = Consultas::listPositionByLevel($conn);
 
   // Función recursiva para obtener subordinados de un supervisor
   function getSubordinates($userId, $user) {
@@ -8,16 +12,17 @@
       
       // Filtramos los usuarios cuyo supervisor sea el 'userId' actual
       foreach ($user as $u) {
-          if ($u['superuserId'] == $userId) {
+          if ($u['PuestoSup'] == $userId) {
               // Creamos el nodo del subordinado
               $subordinate = [
-                  'id' => $u['usuarioId'],
+                  'id' => $u['numEmpleado'], 
+                  'name' => $u['puestoId'],
                   'name' => $u['puesto'],
                   'description' => $u['funciones'],
-                  'parent_id' => $u['superuserId'],
+                  'parent_id' => $u['PuestoSup'],
                   'staff_department' => '',
-                  'manager_id' => $u['usuarioId'],
-                  'children' => getSubordinates($u['usuarioId'], $user) // Recursión para obtener subordinados de este subordinado
+                  'manager_id' => $u['numEmpleado'],
+                  'children' => getSubordinates($u['puestoId'], $user) // Recursión para obtener subordinados de este subordinado
               ];
 
               $subordinates[] = $subordinate;
@@ -36,13 +41,14 @@
 
     <?php foreach ($userlvl0 as $un0) { ?>
       {
-        id: "<?= $un0['usuarioId'] ?>",
+        id: "<?= $un0['numEmpleado'] ?>",
+        name: "<?= $un0['puestoId'] ?>",
         name: "<?= $un0['puesto'] ?>",
         description: "<?= $un0['funciones'] ?>",
         parent_id: '',
         staff_department: '',
-        manager_id: "<?= $un0['usuarioId'] ?>",
-        children: <?= json_encode(getSubordinates($un0['usuarioId'], $user)) ?>, // Aquí llamamos a la función recursiva para obtener subordinados
+        manager_id: "<?= $un0['numEmpleado'] ?>",
+        children: <?= json_encode(getSubordinates($un0['puestoId'], $user)) ?>, // Aquí llamamos a la función recursiva para obtener subordinados
         showChildren: true,
       },
     <?php } ?>
@@ -50,7 +56,7 @@
     people: [
       <?php foreach ($user as $usuario) { ?>
         {
-          id: "<?= $usuario['usuarioId'] ?>",
+          id: "<?= $usuario['numEmpleado'] ?>",
           name: "<?= $usuario['nombre'] . " " . $usuario['apellido1'] . " " . $usuario['apellido2'] ?>",
           photo: "<?= $usuario['ruta'] ?>",
         },
